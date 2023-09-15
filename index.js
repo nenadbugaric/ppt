6beb5992126db8bd1f5cbc67127400d2de1076e6
Nenad Bugarić <n.bugaric@gmail.com>
2023-09-15 11:59:07 +0200

    wip

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const puppeteer = require("puppeteer");
 
-const url = "https://corepresent-staging.kvcore.com/ssr/DpxFmptJ532m";
+// const url = "https://corepresent-staging.kvcore.com/ssr/DpxFmptJ532m";
+const url = "http://localhost:3001/ssr/DpxFmptJ532m";
+// const url = "https://goo.gl/maps/yuhNeAZaSEEKAZcD8";
+
+function delay(time, cb) {
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      cb?.();
+      resolve();
+    }, time);
+  });
+}
 
 (async function () {
   try {
     const browser = await puppeteer.launch({
-      // args: chromium.args,
+      // args,
       defaultViewport: null,
       headless: "new",
       ignoreHTTPSErrors: true,
       dumpio: true,
+      // devtools: true,
     });
 
     const page = await browser.newPage();
+
+    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
     await page.setViewport({
       width: 1700,
@@ -24,14 +38,31 @@ const url = "https://corepresent-staging.kvcore.com/ssr/DpxFmptJ532m";
       timeout: 0,
     });
 
-    // await page.emulateMediaType("print");
+    await page.emulateMediaType("print");
     await page.evaluateHandle("document.fonts.ready");
 
+    const mapsHandle = await page.$$(".google-map");
+    console.log("MAPS OUTSIDE: ", mapsHandle?.length);
+
+    if (mapsHandle?.length) {
+      for (const mapHandle of mapsHandle) {
+        await delay(1000);
+        await page.evaluate((map) => {
+          console.log("MAP: ", map);
+
+          map.scrollIntoView();
+        }, mapHandle);
+      }
+
+      await delay(2000);
+    }
+
+    console.log("GENERATING PDF");
     await page.pdf({
       width: "17in",
       height: "22in",
       printBackground: true,
-      preferCSSPageSize: true,
+      preferCSSPageSize: false,
       path: "output.pdf",
     });
 
